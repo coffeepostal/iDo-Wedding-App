63b013ecd4d4697a2159684c2f599f0873bf694d
Dallas Reedy <code@dallasreedy.com>
2009-08-06 16:45:45 -0700

    making it easier to display addresses as HTML; added Address#lines and AddressHelper#html_for_address

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,7 +4,11 @@ class Address < ActiveRecord::Base
   # Validating the address form fields aren't empty.
   validates_presence_of :line_1, :city, :state, :zip
   
+  def lines
+    [line_1, line_2, "#{city}, #{state} #{zip}"]
+  end
+  
   def to_s
-    [line_1, line_2, "#{city}, #{state} #{zip}"].compact.join('<br />')
+    lines.compact.join(', ')
   end
 end
