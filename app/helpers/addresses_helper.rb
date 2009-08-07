e27a2104ff64f2482fe2b3bdac68f56955d718c6
Adam Farnsworth <bulk@adamfarnsworth.com>
2009-08-06 18:21:09 -0700

    All sorts of styling and layout changes...

--- app/helpers/addresses_helper.rb
+++ app/helpers/addresses_helper.rb
@@ -1,2 +1,9 @@
 module AddressesHelper
+  def html_for_address(address, wrapper_tag = :ul, line_tag = :li, html_options = {})
+    content_tag_for(wrapper_tag, address, html_options) do
+      address.lines.map do |line|
+        content_tag(line_tag, line) unless line.blank?
+      end.compact.join
+    end
+  end
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,10 +1,14 @@
 class Address < ActiveRecord::Base
   belongs_to :guest
-  
+
   # Validating the address form fields aren't empty.
   validates_presence_of :line_1, :city, :state, :zip
-  
+
+  def lines
+    [line_1, line_2, "#{city}, #{state} #{zip}"]
+  end
+
   def to_s
-    [line_1, line_2, "#{city}, #{state} #{zip}"].compact.join('<br />')
+    lines.compact.join(', ')
   end
 end
