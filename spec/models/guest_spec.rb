6bf1deecf538fc808fc8e729f6e3fbc5d5132f90
Dallas Reedy <code@dallasreedy.com>
2011-09-14 20:40:18 -0700

    added test for validating the presence of Guest#last_name

--- spec/models/guest_spec.rb
+++ spec/models/guest_spec.rb
@@ -9,6 +9,8 @@ describe Guest do
   it { should respond_to :gift_attributes= }
   it { should respond_to :rsvp_attributes= }
 
+  it { should validate_presence_of :last_name }
+
   it { should respond_to :honorific }
   it { should respond_to :first_name }
   it { should respond_to :last_name }
